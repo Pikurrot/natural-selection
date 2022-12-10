be1ccdf5102e400a8a2c5e926b35d21be2a99768
Eric Lopez <pikurrot@gmail.com>
2022-12-10 19:51:02 +0100

    Define activation functions

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,12 @@
 #include "functions.h"
+#include <cmath>
+
+inline double activationFunction1(double x)
+{
+	return 1/(1+pow(M_E,x));
+}
+
+inline double activationFunction2(double x)
+{
+	return (pow(M_E,x)-pow(M_E,-x))/(pow(M_E,x)+pow(M_E,-x));
+}
--- functions.h
+++ functions.h
@@ -1,1 +1,5 @@
 #pragma once
+
+inline double activationFunction1(double x);
+
+inline double activationFunction2(double x);
